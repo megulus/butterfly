95a2263e343e9ef31252e84fa45951f169cd70df
Meg Dahlgren <meg.dahlgren@gmail.com>
2016-08-05 14:27:34 -0400

    add triangle to question view

--- project/js/views.js
+++ project/js/views.js
@@ -22,7 +22,6 @@ views.FooterView = Backbone.View.extend({
 
 views.SelectedMoodView = Backbone.View.extend({
     template: _.template('<div id="smiley-box"><img id="smiley" alt="<%= alt %>" src="<%= imgPath %>"><img id="edit" src="images/Edit.png"></div>'+
-            //'<div class="row"><div class="col-md-4">Grump</div></div>'),
         '<div class="row mood-text-box"><div class="col-md-8" id="mood-text"><p><h5 class="white-text"><%= label %></h5></p>'
        + '<p class="ltgrn-text" id="thanks">THANK YOU FOR YOUR FEEDBACK</p></div></div>'),
     events: {
@@ -103,7 +102,7 @@ views.QuestionsView = Backbone.View.extend({
         var that = this;
         this.$el.html('');
         if (!(this.model.get('submitted'))) {
-            this.$el.append('<div class="qn-banner"><p class="white-text">Your answers will always remain anonymous.</p></div>' +
+            this.$el.append('<div class="banner-box"><div class="triangle"><img src="images/Triangle.png"></div><div class="qn-banner"><p class="white-text">Your answers will always remain anonymous.</p></div></div>' +
                 '<div><p class="qn-container ltgrn-text">Do you agree with the following statements?</p></div>');
             _.each(this.model.get('questions'), function (question, index) {
                 var singleQuestionView = new views.SingleQuestionView({

dd5bf8b815f653f50424e76623873dd7bf85159f
Meg Dahlgren <meg.dahlgren@gmail.com>
2017-01-12 11:22:02 -0500

    add Submitted component css

--- project/js/components/Submitted/Submitted.js
+++ project/js/components/Submitted/Submitted.js
@@ -1,14 +1,15 @@
-import React, {Component, PropTypes} from 'react';
-import classNames from 'classnames';
+import React, { Component } from 'react';
+
+import styles from './Submitted.css';
 
 class Submitted extends Component {
 
     render() {
         return (
-            <div className={classNames('top-spacer')}>
-                <p className={classNames('top-spacer', 'lt-grn-text', 'small-text')}>THANK YOU FOR THE EXTRA FEEDBACK</p>
-                <p className={classNames('bold', 'white-text')}>Have a nice day!</p>
-                <div className={classNames('submitted')}></div>
+            <div className={styles.submitted}>
+                <p className={styles.banner}>THANK YOU FOR THE EXTRA FEEDBACK</p>
+                <p>Have a nice day!</p>
+                <div></div>
             </div>
         );
     }
